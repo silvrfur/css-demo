f19ce33e48083e24f9f5f9411eb924eac8c22209
silvrfur <silverfur03@gmail.com>
2023-12-23 16:01:55 +0530

    added a fullstack gym membership login form

--- Express/appvid73.js
+++ Express/appvid73.js
@@ -1,3 +1,5 @@
+//import fs
+const fs=require('fs');
 //import express
 const express = require("express");
 //to set path..
@@ -11,7 +13,7 @@ const port = 80;
 //EXPRESS CONFIGS:
 //serving static files
 app.use('/static',express.static('static'));
-
+app.use(express.urlencoded())
 
 //PUG CONFIGS:
 //set the template engine for pug(syntax from express website)
@@ -24,6 +26,19 @@ app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res)=>{
     const con="This is the content"
     const params={'title':'Using pug','content':con}
+    res.status(200).render('index.pug',params)
+})
+app.post('/',(req,res)=>{
+    // console.log(req.body);
+    name=req.body.name; 
+    age=req.body.age; //body.age comes from value of name provided in the input tag
+    gender=req.body.gender; 
+    address=req.body.address; 
+    more=req.body.more;
+
+    let outputToWrite=`the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her/them: ${more}`
+    fs.writeFileSync('output.txt',outputToWrite);
+    const params={message:"Your form has been submitted successfully"}
     res.status(200).render('index.pug',params)
 })
 
